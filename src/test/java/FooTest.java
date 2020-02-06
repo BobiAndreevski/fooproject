9ec5776e5a5623f31ef2df1042f9e978bebe43ca
Bobi Andreevski <bob.andreevski@gmail.com>
2020-02-06 12:58:33 +0000

    13:58

---
+++ src/test/java/FooTest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FooTest {
+
+    @org.junit.jupiter.api.Test
+    void myMethod() {
+        assertEquals(true, Foo.myMethod());
+    }
+}
