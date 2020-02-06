f65d723a938434db99e51e82c4501813e950b118
Bobi Andreevski <bob.andreevski@gmail.com>
2020-02-06 14:20:15 +0000

    15:20

--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -1,7 +1,7 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FooTest {
-
+// kollar om detta funkar
     @org.junit.jupiter.api.Test
     void myMethod() {
         assertEquals(true, Foo.myMethod());
